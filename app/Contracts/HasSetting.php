2ff859f45f0f3af846a93d34426f23671ee0b551
JalalLinuX <smjjalalzadeh93@gmail.com>
2021-07-19 19:31:42 +0430

    Update HasSetting.php

--- app/Contracts/HasSetting.php
+++ app/Contracts/HasSetting.php
@@ -8,6 +8,6 @@ trait HasSetting
 {
     public static function setting($key, $fresh = false)
     {
-        return _settings(self::class, $key, $fresh);
+        return _settings(model_setting_name(self::class), $key, $fresh);
     }
 }
